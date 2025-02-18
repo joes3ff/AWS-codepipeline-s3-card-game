384cf1fff035040474d669767690019e55f57a70
Joe <100196829+joes3ff@users.noreply.github.com>
2025-02-18 12:21:21 +0000

    Update script.js
    went back to the older version.

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/burger.jpg' },
-        { name: 'card1', img: 'images/burger.jpg' },
-        { name: 'card2', img: 'images/car.jpg' },
-        { name: 'card2', img: 'images/car.jpg' },
-        { name: 'card3', img: 'images/dog.jpg' },
-        { name: 'card3', img: 'images/dog.jpg' },
-        { name: 'card4', img: 'images/flower.jpg' },
-        { name: 'card4', img: 'images/flower.jpg' },
-        { name: 'card5', img: 'images/pumpkin.jpg' },
-        { name: 'card5', img: 'images/pumpkin.jpg' },
+        { name: 'card1', img: 'images/distracted.png' },
+        { name: 'card1', img: 'images/distracted.png' },
+        { name: 'card2', img: 'images/drake.png' },
+        { name: 'card2', img: 'images/drake.png' },
+        { name: 'card3', img: 'images/fine.png' },
+        { name: 'card3', img: 'images/fine.png' },
+        { name: 'card4', img: 'images/rollsafe.png' },
+        { name: 'card4', img: 'images/rollsafe.png' },
+        { name: 'card5', img: 'images/success.png' },
+        { name: 'card5', img: 'images/success.png' },
         // ...add more pairs as needed
     ];
 
@@ -29,19 +29,13 @@ document.addEventListener('DOMContentLoaded', () => {
         cardsWon = [];
 
         for (let i = 0; i < cardArray.length; i++) {
-            const cardContainer = document.createElement('div');  // Create card div
-            cardContainer.classList.add('card');
-        
-            const card = document.createElement('img');  // Create image inside the card
+            const card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
             card.setAttribute('data-id', i);
             card.addEventListener('click', flipCard);
-        
-            cardContainer.appendChild(card);  // Append image inside the card
-            grid.appendChild(cardContainer);  // Append the card to the grid
+            grid.appendChild(card);
         }
     }
-
 
     function flipCard() {
         let cardId = this.getAttribute('data-id');

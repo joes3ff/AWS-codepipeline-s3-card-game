ffa3b3b0a2b14455269d4879298b55dc1c0d6107
Joe <100196829+joes3ff@users.noreply.github.com>
2025-02-18 12:10:30 +0000

    Update script.js
    updated the js to use new images.

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card1', img: 'images/burger.jpg' },
+        { name: 'card1', img: 'images/burger.jpg' },
+        { name: 'card2', img: 'images/car.jpg' },
+        { name: 'card2', img: 'images/car.jpg' },
+        { name: 'card3', img: 'images/dog.jpg' },
+        { name: 'card3', img: 'images/dog.jpg' },
+        { name: 'card4', img: 'images/flower.jpg' },
+        { name: 'card4', img: 'images/flower.jpg' },
+        { name: 'card5', img: 'images/pumpkin.jpg' },
+        { name: 'card5', img: 'images/pumpkin.jpg' },
         // ...add more pairs as needed
     ];
 
